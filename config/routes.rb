7343c73cf0489aace9eff9cbba6ea215eaab9158
Mihai Macovei <mihaimacovei@Mihais-Mac-mini.local>
2014-10-03 14:05:37 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
+  get "static_pages/" => redirect("static_pages/home")
   get "static_pages/help"
   get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
